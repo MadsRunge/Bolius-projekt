59efdb823b53e68aab71a277b3cf752e8efb7663
madsen10 <91535029+madsen10@users.noreply.github.com>
2022-12-01 10:31:24 +0100

    chart +

--- main.js
+++ main.js
@@ -7,19 +7,20 @@ const chartAOS = new Chart(ctx4, {
     data: {
         labels: ["2000", "2001", "2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021","2022"],
         datasets: [{
-            label: 'Bolig - Gennensnitlige pris pr. m2',
+            label: 'Danmark',
             data: [10204, 11523, 12525, 13491,14710,17304,22654,22371,20633,16799,17970,18577,17461,18429,19439,21261,23270,25146,27416,27544,28678,32105,34469],
-            borderColor: ['red'],
+            borderColor: '#EE5C47',
         },
         ]
 
     },
     options: {
+
         scales: {
             x: {
                 title: {
                     display: true,
-                    text: 'År 2000-2022'
+                    text: '2000-2022'
                 },
                 grid: {
                     display: false
@@ -28,7 +29,7 @@ const chartAOS = new Chart(ctx4, {
             y: {
                 title: {
                     display: true,
-                    text: 'Hours'
+                    text: 'Gennensnitlige pris pr. m2'
                 },
                 grid: {
                     display: false
@@ -41,7 +42,7 @@ const chartAOS = new Chart(ctx4, {
             },
             title: {
                 display: true,
-                text: 'Aircraft out of service due to birdstrikes'
+                text: " Udvikling i salgskvadratmeterpris for hele landet  "
             }
         }
     },
