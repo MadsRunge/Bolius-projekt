b4f59a80f2511a7d7c366087bca302f0f5ad0fdb
madsen10 <91535029+madsen10@users.noreply.github.com>
2022-12-08 09:21:09 +0100

    begge chart

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const ctx = document.querySelector('#chart').getContext('2d');
 ctx.canvas.width = 1
 ctx.canvas.height = 1
-const chartAOS = new Chart(ctx, {
+const chartPrice = new Chart(ctx, {
     type: 'line',
     data: {
         labels: ["2000", "2001", "2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021","2022"],
@@ -60,7 +60,7 @@ const chart = new Chart(ctx2, {
         labels: [],
         datasets: [{
             label: "Sammenligningsgraf",
-            data: [],
+            data: [10,10],
         }]
     }
 });
